df04fff07a99178acf42abff97b4054ec27068a9
isk-bokang <bo.kang@iskra.world>
2022-05-26 11:01:59 +0900

    Add Network Change Events

--- src/App.tsx
+++ src/App.tsx
@@ -20,19 +20,19 @@ interface AddEthereumChainParameter {
 
 
 declare global {
-  interface Window{
-    ethereum?:MetaMaskInpageProvider
+  interface Window {
+    ethereum?: MetaMaskInpageProvider
   }
 }
-let accountAddr  = "";
+let accountAddr = "";
 
-const goerli : AddEthereumChainParameter ={
+const goerli: AddEthereumChainParameter = {
   chainId: toHex(5)
 }
 
-const Klaytn : AddEthereumChainParameter ={
+const Klaytn: AddEthereumChainParameter = {
   chainId: toHex(8217),
-  blockExplorerUrls : ["https://scope.klaytn.com"],
+  blockExplorerUrls: ["https://scope.klaytn.com"],
   chainName: "Klaytn Mainnet Cypress",
   rpcUrls: ["https://public-node-api.klaytnapi.com/v1/cypress"],
 }
@@ -43,41 +43,72 @@ const Klaytn : AddEthereumChainParameter ={
 async function addNetwork() {
 
   console.log(Klaytn)
-   const res = await window.ethereum?.request({ method : "wallet_addEthereumChain", params : [Klaytn]})
+  const res = await window.ethereum?.request({ method: "wallet_addEthereumChain", params: [Klaytn] })
+
   return res;
 
 }
 
-async function switchNetwork(){
+async function switchNetwork() {
   console.log(goerli)
-   const res = await window.ethereum?.request({ method : "wallet_switchEthereumChain", params : [goerli]})
+  const res = await window.ethereum?.request({ method: "wallet_switchEthereumChain", params: [goerli] })
   return res;
 }
 
-async function requestAccount(){
-  if(window.ethereum){
-    const accounts  = await window.ethereum.request({method : 'eth_requestAccounts'});
+async function requestAccount() {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     accountAddr = typeof accounts === 'string'
-        ? accounts
-        : "";
+      ? accounts
+      : "";
   }
-  else{
+  else {
     console.log("I Need Metamask")
   }
 }
 
-
+function HandleChangeNetworkEvent() {
+  if (window.ethereum?.chainId !== goerli.chainId && window.ethereum?.isConnected()) {
+    alert(" Need To Change Network to Goerli")
+    console.log(switchNetwork());
+    window.location.reload();
+  }
+}
 
 
 
 
 function App() {
 
+  useEffect(() => {
+    window.ethereum?.on('connect', () => { console.log("Connect Event"); });
+    window.ethereum?.on('disconnect', () => { console.log('Disconnect Event'); })
+
+    window.ethereum?.on('accountsChanged', () => {
+      console.log("Account Changed Event");
+      HandleChangeNetworkEvent();
+    });
+
+    window.ethereum?.on('chainChanged', () => {
+      console.log('chainChanged Event');
+      HandleChangeNetworkEvent();
+    })
+
+    return () => {
+      window.ethereum?.removeAllListeners('connect');
+      window.ethereum?.removeAllListeners('disconnect');
+      window.ethereum?.removeAllListeners('accountsChanged');
+      window.ethereum?.removeAllListeners('chainChanged');
+
+    }
+  })
+
+
   return (
     <div className="App">
       {< button onClick={requestAccount}> Connect </button>}
-      { <button onClick={switchNetwork}> Switch To Goerli</button>}
-      { <button onClick={addNetwork}> Add Klaytn </button>}
+      {<button onClick={switchNetwork}> Switch To Goerli</button>}
+      {<button onClick={addNetwork}> Add Klaytn </button>}
     </div>
   );
 
